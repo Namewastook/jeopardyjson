89b1e890ede5696df677f6d8690cc69b7116f31f
Michael McKenzie <mikemckenzie987@gmail.com>
2018-10-04 14:44:44 -0400

    done

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const HTTPClient = require('./httpClient.js');
 const fs = require('fs');
-const url = 'http://jservice.io/api/category?id=';
+let url = 'http://jservice.io/api/category?id=';
 const categoriesArray = [777, 780, 21, 105, 25, 306, 136, 680, 309, 2537, 1470, 18065, 6145, 9646, 15090, 499, 42, 7, 78, 211, 420, 176];
 
 const categoryPromises = categoriesArray.map(id => HTTPClient(url + id))
